f261520db5eef98a7e0dcc17d77aaade455aef05
DadiT <76049051+Dadi-T@users.noreply.github.com>
2022-02-25 10:18:26 +0100

    Update web3.ts

--- ethereum/web3.ts
+++ ethereum/web3.ts
@@ -6,7 +6,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   web3 = new Web3(window.ethereum);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/4a917d78d0aa477a8d9408b8e75691ca"
+    ""
   );
   web3 = new Web3(provider);
 }
